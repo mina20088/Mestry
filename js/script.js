6799f76fa72bf5e55b53c70d03434cd6b7d5480c
mina remon <minakiroollos@gmail.com>
2024-09-08 01:52:42 +0300

    changes made to the contact

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@
 const navbar = document.getElementsByClassName('navbar');
 
 window.addEventListener("scroll", function (e) {
-       if(this.scrollY > 50){
-           navbar.item(0).classList.add('bg-dark')
-           navbar.item(0).classList.add('opacity-9');
-           navbar.item(0).classList.add('transation')
-       }else{
-           navbar.item(0).classList.remove('bg-dark');
-           navbar.item(0).classList.remove('opacity-9');
-           navbar.item(0).classList.remove('transation')
-       }
+    if (this.scrollY > 50) {
+        navbar.item(0).classList.add('bg-dark')
+        navbar.item(0).classList.add('opacity-9');
+        navbar.item(0).classList.add('transation')
+    } else {
+        navbar.item(0).classList.remove('bg-dark');
+        navbar.item(0).classList.remove('opacity-9');
+        navbar.item(0).classList.remove('transation')
+    }
 })
